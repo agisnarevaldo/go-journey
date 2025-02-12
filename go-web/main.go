cc5097f7c1b3f0854a9b12957e84c49bed6c7cda
Agisna Revaldo <agisrifaldo75@gmail.com>
2025-02-13 02:55:12 +0700

    add simple http

---
+++ go-web/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func ()  {
+  http.HandleFunc("/", func (w http.ResponseWriter, r *httpRequest)  {
+    fmt.Fprintf(w, http.ResponseWriter, r *http.Request)
+  })
+}
