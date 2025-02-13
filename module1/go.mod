270f5aa20042af09ef67edff69edc8a7b4cc8d3d
Agisna Revaldo <agisrifaldo75@gmail.com>
2025-02-13 17:29:59 +0700

    add module1 with greetings package and main function

---
+++ module1/go.mod
@@ -0,0 +1,5 @@
+module module1
+
+go 1.24.0
+
+require github.com/agisnarevaldo/greetings v0.1.0 // indirect
---
+++ module1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/agisnarevaldo/greetings"
+)
+
+func main() {
+	fmt.Println(greetings.Hello("World"))
+}
