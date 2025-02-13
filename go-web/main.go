c0e3a2d4246af99f94c6e8dc1d51de93468a8b5e
Agisna Revaldo <agisrifaldo75@gmail.com>
2025-02-13 17:29:41 +0700

    refactor main function to improve logging and change server port to 8080

--- go-web/main.go
+++ go-web/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func ()  {
-  http.HandleFunc("/", func (w http.ResponseWriter, r *httpRequest)  {
-    fmt.Fprintf(w, http.ResponseWriter, r *http.Request)
-  })
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("Received request for: ", r.URL.Path)
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	log.Print("Starting server on port 8080")
+	err := http.ListenAndServe(":8080", nil) // Changed port from 80 to 8080
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
