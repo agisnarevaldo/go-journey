c437c83d61b1655f61d00b819d4a5ce7e4349c6d
Agisna Revaldo <agisrifaldo75@gmail.com>
2025-03-06 02:24:20 +0700

    belajar tipe data slice

---
+++ get-started/slice.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+func main() {
+	months := [12]string{
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	}
+
+	var slice1 []string = months[4:7]
+	fmt.Println(slice1)
+
+	slice2 := months[:3]
+	fmt.Println(slice2)
+
+	slice3 := months[10:]
+	fmt.Println(slice3)
+
+	slice4 := months[:]
+	fmt.Println(slice4)
+
+	slice2[0] = "Januari"
+	slice2[1] = "Februari"
+	fmt.Println(slice2)
+	fmt.Println(months)
+
+	slice5 := append(slice4, "Happy New Year")
+	fmt.Println(slice5)
+	fmt.Println(slice4)
+
+	var newSlice []string = make([]string, 3, 5)
+	newSlice[0] = "Muhamad"
+	newSlice[1] = "Agisna"
+	newSlice[2] = "Revaldo"
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	newSlice2 := append(newSlice, "Paleee")
+	fmt.Println(newSlice2)
+	fmt.Println(len(newSlice2))
+	fmt.Println(cap(newSlice2))
+	newSlice2 = append(newSlice2, "Brian")
+	fmt.Println(newSlice2)
+	fmt.Println(len(newSlice2))
+	fmt.Println(cap(newSlice2))
+
+	toSlice := make([]string, len(newSlice2), cap(newSlice2))
+	copy(toSlice, newSlice2)
+
+	fmt.Println(toSlice)
+	fmt.Println(len(newSlice2))
+	fmt.Println(cap(newSlice2))
+}
